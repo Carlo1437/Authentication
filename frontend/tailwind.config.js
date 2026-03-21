ea89c67e6ab29bf9164a89797648df2f1416e906
Carlo1437 <camarote757@gmail.com>
2026-03-21 16:24:49 +0800

    fix: tailwind styling

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,25 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
-  content: [],
+  darkMode: "class",
+  content: [
+    "./components/**/*.{vue,js,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./app.vue",
+    "./plugins/**/*.{js,ts}",
+  ],
   theme: {
     extend: {
       colors: {
-        primary: '#137fec',
-        secondary: '#10b981',
-        'background-light': '#f6f7f8',
-        'background-dark': '#101922',
+        primary: "#137fec",
+        secondary: "#10b981",
+        "background-light": "#f6f7f8",
+        "background-dark": "#101922",
       },
       fontFamily: {
-        display: ['Public Sans', 'sans-serif'],
+        display: ["Public Sans", "sans-serif"],
       },
       borderRadius: {
-        DEFAULT: '0.5rem',
-        lg: '1rem',
-        xl: '1.5rem',
-        full: '9999px',
+        DEFAULT: "0.5rem",
+        lg: "1rem",
+        xl: "1.5rem",
+        full: "9999px",
       },
     },
   },
   plugins: [],
-}
+};
