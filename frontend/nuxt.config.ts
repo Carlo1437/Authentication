53319c459d19c3bd5380100b96909f29bf5fb56d
Carlo1437 <camarote757@gmail.com>
2026-03-21 16:41:54 +0800

    fix

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,9 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: false },
-  css: ["~/assets/css/main.css"],
   modules: ["@nuxtjs/tailwindcss"],
-
+  tailwindcss: {
+    configPath: "tailwind.config.js",
+  },
   devServer: {
     host: "localhost",
   },
